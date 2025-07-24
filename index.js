ed59debf6a80a05e212cba04bb7bf70adecc3ebc
Rifat-Earth <rifathasan01915@gmail.com>
2025-07-24 12:55:48 +0600

    add blood donors

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hols6da.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("bloodCollectionDB");
+        const donationsCollection = database.collection("donations");
+
+        app.post("/add-blood", async (req, res) => {
+            const donation = req.body;
+            const result = await donationsCollection.insertOne(donation);
+            res.send(result);
+        });
+
+
+        app.get("/donations", async (req, res) => {
+            const donations = await donationsCollection.find().toArray();
+            res.send(donations);
+        });
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('running on server port 5000')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
