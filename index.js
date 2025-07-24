b6f9a11921d2bce4c3189f15dc3e68e39d95e4ea
Rifat-Earth <rifathasan01915@gmail.com>
2025-07-24 14:08:34 +0600

    last push

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 
